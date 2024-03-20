872b12ddfb36d222412a1b8ea0e14150df84b0e4
andrew <cole.zubac@gmail.com>
2024-03-20 15:40:17 -0400

    function to initialize app on indes.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquire = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquire.createPromptModule(questions)
+    .then((inquireResponses) => {
+        console.log('Creating ReadMe');
+        writeToFile('README.md', generateMarkdown((... inquireResponses)));
+    });
+}
 
 // Function call to initialize app
 init();
